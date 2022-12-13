6b9f4e55a6f1a0d4b29a4771b9a00cd13dd47c6a
haydn <haydnt@zohomail.com>
2022-12-13 10:14:07 -0800

    finds my display on my thinkpad

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::process::Command;
-
-fn main()->Result<(), ()> {
-
+use xrandr::{XHandle, XrandrError, Monitor};
+fn main()->Result<(), XrandrError> {
     let output =
         Command::new ("xrandr").status().expect("failed to execute process");
+    let monitors = XHandle::open() ?.monitors() ? ;
+    let mut main_display: Option<&Monitor> = None;
+
+   for i in &monitors {
+            if i .is_primary { main_display = Some(i); }
+       }
+
+        panic!("Cannot find any displays!");
+    
 
     Ok(())
 }
