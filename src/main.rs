3f3f9180d3b7ec0574148f0ab09c7b422481400b
haydn <haydnt@zohomail.com>
2022-12-17 15:59:38 -0800

    error handeling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
-use std::process::Command;
-use xrandr::{XHandle, XrandrError, Monitor};
-fn main()->Result<(), XrandrError> {
-    let output =
-        Command::new ("xrandr").status().expect("failed to execute process");
-    let monitors = XHandle::open() ?.monitors() ? ;
+use xrandr::{XHandle, Monitor};
+
+ mod daemon;
+
+fn main() {
+   
+
+    //might move the following code to another function
+
     let mut main_display: Option<&Monitor> = None;
+   
 
-   for i in &monitors {
+    //get a list of the displays on the system
+    let monitors = XHandle::open().unwrap()
+    .monitors().unwrap();
+
+    //determine main display
+    for i in &monitors {
             if i .is_primary { main_display = Some(i); }
-       }
+            break;//dont need to look anymore at this point 
+   }
 
-        panic!("Cannot find any displays!");
+    
+    
+    match main_display{
+    Some(disp)=>{daemon::run(&disp.name).expect("Error with Daemon")},
+    None => eprintln!("Could not find a display!")
+    }
     
 
-    Ok(())
 }
+
+
+
+
