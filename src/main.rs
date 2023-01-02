527822332d874b8a4487d2efbfd5dd3e7bf9ceab
haydn <haydnt@zohomail.com>
2023-01-01 21:26:46 -0800

    pub crate and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,25 @@
-use xrandr::{XHandle, Monitor};
+pub(crate) use xrandr::{Monitor, XHandle};
 
- mod daemon;
+mod daemon;
 
 fn main() {
-   
-
     //might move the following code to another function
 
     let mut main_display: Option<&Monitor> = None;
-   
 
     //get a list of the displays on the system
-    let monitors = XHandle::open().unwrap()
-    .monitors().unwrap();
+    let monitors = XHandle::open().unwrap().monitors().unwrap();
 
     //determine main display
     for i in &monitors {
-            if i .is_primary { main_display = Some(i); }
-            break;//dont need to look anymore at this point 
-   }
-
-    
-    
-    match main_display{
-    Some(disp)=>{daemon::run(&disp.name).expect("Error with Daemon")},
-    None => eprintln!("Could not find a display!")
+        if i.is_primary {
+            main_display = Some(i);
+        }
+        break; //dont need to look anymore at this point
     }
-    
 
+    match main_display {
+        Some(disp) => daemon::run(&disp.name).expect("Error with Daemon"),
+        None => eprintln!("Could not find a display!"),
+    }
 }
-
-
-
-
