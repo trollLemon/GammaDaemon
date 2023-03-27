3527f6e2f93fcf1ff43edac6f7ae25bdcfee3a41
haydn <haydnt@zohomail.com>
2023-03-26 23:08:32 -0700

    updated mods and added unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 
 mod daemon;
 use bulbb::monitor::MonitorDevice;
-use battery::Error;
 fn main() {
 
     //find monitor device 
     let monitors = MonitorDevice::get_all_monitor_devices().unwrap();
     
     //get a ref to the first monitor in the list of monitors
-    let main_Monitor : &MonitorDevice = &monitors[0];
+    let main_monitor : &MonitorDevice = &monitors[0];
 
-     let mut process: daemon::Daemon = daemon::Daemon::new(main_Monitor);
+     let mut process: daemon::Daemon = daemon::Daemon::new(main_monitor);
     
-      process.run(); 
+      process.run().unwrap(); 
      
 
 }
