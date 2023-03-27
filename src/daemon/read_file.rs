891c4d1163294d25874ea2b2fbb96c8b1298aded
haydn <haydnt@zohomail.com>
2023-03-26 23:08:57 -0700

    file reader helper struct

---
+++ src/daemon/read_file.rs
@@ -0,0 +1,17 @@
+
+use std::fs;
+use std::io;
+
+pub struct ReadFile;
+
+
+impl ReadFile {
+
+
+
+     pub fn get_contents(path: &str) -> io::Result<String> {
+        let data = fs::read_to_string(path)?;
+        Ok(data)
+    }
+}
+
