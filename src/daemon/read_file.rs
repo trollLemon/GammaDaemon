ee054bb68b34617253178879750511311f62d22a
Haydn <haydnt@zohomail.com>
2023-04-11 08:39:31 -0700

    removed struct because it doesn't need to be in the code

--- src/daemon/read_file.rs
+++ src/daemon/read_file.rs
@@ -1,16 +1,13 @@
 
 use std::fs;
 use std::io;
-pub struct ReadFile;
-
-
-impl ReadFile {
 
 
 
-     pub fn get_contents(path: &str) -> io::Result<String> {
+
+
+    pub fn get_contents(path: &str) -> io::Result<String> {
         let data = fs::read_to_string(path)?;
         Ok(data)
     }
-}
 
