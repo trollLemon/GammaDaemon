ba909ef71b1a2cfb3004b16b1da6ceb264fd811e
Haydn <haydnt@zohomail.com>
2023-09-19 20:04:20 -0700

    support for custom config file location

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod daemon;
 use bulbb::monitor::MonitorDevice;
+use std::env;
+
 fn main() {
     //find monitor device
     let monitors = MonitorDevice::get_all_monitor_devices().unwrap();
-
+    
+    let mut args: Vec<String> = env::args().collect();
     //get a ref to the first monitor in the list of monitors
     let main_monitor: &MonitorDevice = &monitors[0];
-
+    
+    if args.len() == 1 {
+        args.push("NAN".to_string());
+    }
     //start the daemon
-    daemon::run(main_monitor).unwrap();
+    daemon::run(main_monitor, &args[1]).unwrap();
 }
