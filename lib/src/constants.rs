88a78b03fb22dd4ae5619501a8ee292f59694e56
Haydn Tamura <haydn.tamura@canonical.com>
2026-05-21 21:40:02 +0000

    moved from workplace structure to single crate structure

--- lib/src/constants.rs
+++
@@ -1,6 +0,0 @@
-//  constants for default values and paths
-pub const DEFAULT_CONFIG_PATH: &str = "/etc/gamma_daemon/config.toml";
-pub const DEFAULT_LOG_LEVEL: &str = "info";
-pub const DEFAULT_LOG_FILE: &str = "/var/log/gamma_daemon.log";
-pub const DEFAULT_RUNTIME_DIR: &str = "/var/run/gamma_daemon";
-pub const DEFAULT_SOCKET_PATH: &str = "/var/run/gamma_daemon/gamma_daemon.sock";
